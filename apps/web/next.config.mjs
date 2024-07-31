08e8a1b1a0ba7f3a294273829e1709ebb65e5784
frixaco <rustam21ashurmatov@gmail.com>
2024-07-31 22:36:52 +0500

    Fix env

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -61,8 +61,8 @@ export default withBundleAnalyzer(
     // For all available options, see:
     // https://github.com/getsentry/sentry-webpack-plugin#options
 
-    org: "rustam-ashurmatov",
-    project: "anitrack",
+    org: process.env.SENTRY_ORG,
+    project: process.env.SENTRY_PROJECT,
 
     // Only print logs for uploading source maps in CI
     silent: !process.env.CI,
@@ -92,4 +92,3 @@ export default withBundleAnalyzer(
     automaticVercelMonitors: true,
   }),
 );
-
