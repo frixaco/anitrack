c31d27fefba03f1d05f1319e09d2b9d2e44d556c
frixaco <rustam21ashurmatov@gmail.com>
2024-07-31 22:13:47 +0500

    Update Sentry setup

--- apps/web/instrumentation.ts
+++ apps/web/instrumentation.ts
@@ -1,19 +1,9 @@
-import * as Sentry from "@sentry/nextjs";
-
-export function register() {
+export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
-    // this is your Sentry.init call from `sentry.server.config.js|ts`
-    Sentry.init({
-      dsn: "https://examplePublicKey@o0.ingest.sentry.io/0",
-      // Your Node.js Sentry configuration...
-    });
+    await import("./sentry.server.config");
   }
 
-  // This is your Sentry.init call from `sentry.edge.config.js|ts`
   if (process.env.NEXT_RUNTIME === "edge") {
-    Sentry.init({
-      dsn: "https://examplePublicKey@o0.ingest.sentry.io/0",
-      // Your Edge Runtime Sentry configuration...
-    });
+    await import("./sentry.edge.config");
   }
 }
