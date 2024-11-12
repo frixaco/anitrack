cf1312c1d845a2cc4a52ca15e938f2e5e4652e54
frixaco <rustam21ashurmatov@gmail.com>
2024-11-12 23:20:53 +0500

    Fetch existing torrent if already exists

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Anitrack - Simple anime release tracker for myself
 
+## TODO
+
+- [ ] Add support for subtitles (MKV are not natively supported, but with ffmpeg+webassembly it might be possible)
+
 ## Setup
 
 - Install Bun
@@ -8,15 +12,3 @@
 bun install
 bun dev
 ```
-
-## HOW IT WORKS
-
-What I want to be able to do?
-
-- Search anime from hianime.to and nyaa.si (SubsPlease)
-- Add anime from results as **tracked**
-- Show a button to mark as **watched**
-- Show a button to open hianime.to episode page
-- Show a button to start streaming from nyaa.si magnet link (SubsPlease, webtorrent)
-- See total number of episodes
-- Watched and unwatched episodes
--- src/app/player/page.tsx
+++ src/app/player/page.tsx
@@ -6,42 +6,65 @@ export default async function Page({
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }) {
   const magnetLink = (await searchParams).url as string;
+  let streamUrl: string | null = null;
 
-  const res = await fetch("https://api.anitrack.frixaco.com/torrents", {
+  let res = await fetch("https://api.anitrack.frixaco.com/torrents", {
     method: "POST",
     body: magnetLink,
   });
-  const torrentInfo = await res.json();
-  console.log(torrentInfo);
 
-  const infoHash = torrentInfo.details.info_hash;
-  const fileIdx = torrentInfo.id;
+  if (res.status === 409) {
+    console.log("torrent already exists...");
+    res = await fetch(`https://api.anitrack.frixaco.com/torrents`);
+    const torrents = (await res.json()).torrents;
+    const torrent = torrents.find((t: { info_hash: string }) =>
+      magnetLink.includes(t.info_hash)
+    );
+    res = await fetch(
+      `https://api.anitrack.frixaco.com/torrents/${torrent.id}`
+    );
+    const torrentInfo = await res.json();
+    console.log("fetched existing torrent...", torrentInfo);
 
-  if (typeof infoHash !== "string") {
-    throw new Error("Invalid infohash");
+    const infoHash = torrentInfo.info_hash;
+    const fileIdx = torrentInfo.files.length - 1; // always one file
+
+    streamUrl = `https://api.anitrack.frixaco.com/torrents/${infoHash}/stream/${fileIdx}`;
+  } else {
+    const torrentInfo = await res.json();
+    console.log("added new torrent...", torrentInfo);
+
+    const infoHash = torrentInfo.details.info_hash;
+    const fileIdx = torrentInfo.details.files.length - 1; // always one file
+
+    if (typeof infoHash !== "string") {
+      throw new Error("Invalid infohash");
+    }
+    if (!Number.isFinite(fileIdx)) {
+      throw new Error("Invalid file index");
+    }
+
+    streamUrl = `https://api.anitrack.frixaco.com/torrents/${infoHash}/stream/${fileIdx}`;
   }
-  if (!Number.isFinite(fileIdx)) {
-    throw new Error("Invalid file index");
-  }
-
-  const streamUrl = `https://api.anitrack.frixaco.com/torrents/${infoHash}/stream/${fileIdx}`;
 
   return (
     <Suspense fallback={<div>loading the player...</div>}>
       <div className="h-screen w-full flex items-center justify-center">
         <div className="w-full max-w-4xl mx-auto">
-          <p className="pb-4 overflow-auto text-sm text-gray-500 line-clamp-2">
+          <p className="pb-8 overflow-auto text-sm text-gray-500 line-clamp-2">
             magnet link: {magnetLink}
           </p>
-          <video
-            controls
-            autoPlay
-            className="w-full rounded-lg shadow-lg bg-black"
-            style={{ aspectRatio: "16/9" }}
-            src={streamUrl}
-          >
-            Your browser does not support the video tag.
-          </video>
+          {streamUrl && (
+            <video
+              controls
+              autoPlay
+              className="w-full rounded-lg shadow-lg bg-black"
+              style={{ aspectRatio: "16/9" }}
+              src={streamUrl}
+            >
+              Your browser does not support the video tag.
+            </video>
+          )}
           <div className="mt-4 text-sm text-gray-500">
             Note: Video may take a few moments to start as the torrent begins
             downloading.
