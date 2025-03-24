ed38d4fb75cb58eea54adbf40ca36e09554729e8
frixaco <rustam21ashurmatov@gmail.com>
2025-03-24 13:22:44 +0500

    Revert Nexj.js app to working state just in case

--- nextjs-app/src/app/layout.tsx
+++ nextjs-app/src/app/layout.tsx
@@ -1,20 +1,8 @@
 import type { Metadata } from "next";
-// import localFont from "next/font/local";
 import { Space_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { ClerkProvider } from "@clerk/nextjs";
-
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
 
 const space_mono = Space_Mono({
   subsets: ["latin"],
