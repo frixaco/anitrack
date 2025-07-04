53f33a96212210e03a5725ab348d2c5ca547e906
frixaco <rustam21ashurmatov@gmail.com>
2025-07-04 19:21:15 +0500

    Init Ratatui

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from anitrack!")
+from textual.app import App
+
+
+class MyApp(App):
+    pass
 
 
 if __name__ == "__main__":
-    main()
+    app = MyApp()
+    app.run()
